6536c15570700994fd458ed051cf699e47503e6e
HatemAbid <hatem.imp@gmail.com>
2021-01-22 14:22:36 +0100

    1 file modified

--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -10,30 +10,68 @@ export class HelloComponent  {
 }
 
 /*
+Test Driven Development :
+http://learnitmyway.com/tdd-example/
 
-Backend-Entwickler  / Spring Boot Fizz Buzz
+Recherche Google : fizzbuzz spring boot
 
- 
+Angular CRUD cvtech:https://github.com/aymensellaouti/cvTech
+
+Backend-Entwickler / Spring Boot Fizz Buzz
+
+Écrivez une petite application Spring Boot avec une API REST, qui reçoit une valeur d'entrée et renvoie une liste de résultats au format JSON!
+
+En entrant un nombre, l'application renvoie une liste de valeurs à partir de 1 jusqu'au nombre saisi en tenant compte des règles suivantes:
+
+Si le nombre est divisible par 3, vous devez sortir Fizz
+Si le nombre est divisible par 5, vous devez générer Buzz
+Si le nombre est divisible par 3 et 5 à la fois, vous devez sortir FizzBuzz
+Sinon, il vous suffit de sortir le numéro suivant
+ 
+
+Conseils: utilisez TDD, expliquez les étapes que vous avez suivies et faites attention aux conditions d'erreur. Il n'est pas nécessaire de développer une interface utilisateur.
+
+ 
+
+Application Frontend-Entwickler / Angular CRUD
+
+Écrivez une petite application angulaire pour gérer les personnes.
+
+Une personne possède les attributs suivants: identifiant, prénom, nom, adresse e-mail.
+
+Vous devriez pouvoir répertorier toutes les personnes par nom, afficher les détails d'une personne, modifier une personne et supprimer une personne de la liste.
+
+Il suffit d'avoir un faux / faux service de backend, mais les demandes / réponses doivent être fournies au format JSON.
+
+ 
+
+Conseils: essayez de suivre une approche TDD et documentez vos étapes de développement et expliquez les décisions que vous avez prises.
+
+
+*********************************************************************************
+Backend-Entwickler  / Spring Boot Fizz Buzz
+
+ 
 
 Write a little Spring Boot Application with an REST-API, that receives an input value and returns a result list in JSON format !
 
 By entering a number the application returns a list of values starting from 1 up the entered number considering the following rules:
 
- 
+ 
 
 If number is divisible by 3, then you need to output Fizz
 If number is divisible by 5, then you need to output Buzz
 If number is divisible by 3 and 5 both, then you need to output FizzBuzz
 Else you just need to output the next number
- 
+ 
 
 Hints: Use TDD, explain the steps you took, and mind error conditions. There is no need to develop a user interface. 
 
- 
+ 
 
-Frontend-Entwickler  / Angular CRUD Application
+Frontend-Entwickler  / Angular CRUD Application
 
- 
+ 
 
 Write a small Angular Application to manage persons.
 
@@ -43,10 +81,8 @@ You should be able to list all persons by name, show details for a person, edit 
 
 It’s sufficient to have a mock/fake backend-service, but requests / responds should be provided in JSON-Format.
 
- 
+ 
 
 Hints: Try to follow an TDD approach and document your development steps and explain the decisions you took.
-
- 
 
  */
