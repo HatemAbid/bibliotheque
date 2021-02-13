21637b1087c41302e5a75988b08a974c7e8adb87
HatemAbid <hatem.imp@gmail.com>
2021-02-13 12:27:32 +0100

    angular-oauth2

--- src/app/auth.config.ts
+++ src/app/auth.config.ts
@@ -27,3 +27,15 @@ export const authCodeFlowConfig: AuthConfig = {
 
   showDebugInformation: true
 };
+
+// import { AuthConfig } from 'angular-oauth2-oidc';
+
+// export const authCodeFlowConfig: AuthConfig = {
+//   issuer: 'https://idsvr4.azurewebsites.net',
+//   redirectUri: window.location.origin + '/index.html',
+//   clientId: 'spa',
+//   responseType: 'code',
+//   scope: 'openid profile email offline_access api',
+//   showDebugInformation: true,
+//   timeoutFactor: 0.01
+// };
