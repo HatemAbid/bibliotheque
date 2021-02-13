758265debdf4a506ed21146cf1f2777306dfb36f
HatemAbid <hatem.imp@gmail.com>
2021-02-13 12:48:40 +0100

    1 file modified

--- src/app/auth.config.ts
+++ src/app/auth.config.ts
@@ -3,6 +3,8 @@ import { AuthConfig } from "angular-oauth2-oidc";
 export const authCodeFlowConfig: AuthConfig = {
   // Url of the Identity Provider
   issuer: "https://www.google.fr",
+  //https://www.linkedin.com/pulse/implicit-flow-authentication-using-angular-ghanshyam-shukla
+  //https://github.com/manfredsteyer/angular-oauth2-oidc 
 
   // URL of the SPA to redirect the user to after login
   redirectUri: window.location.origin + "/index.html",
@@ -27,6 +29,27 @@ export const authCodeFlowConfig: AuthConfig = {
 
   showDebugInformation: true
 };
+
+// // Url of the Identity Provider
+//   issuer: 'https://demo.identityserver.com/identity',
+
+//   // Login Url of the Identity Provider
+//   loginurl: 'https://demo.identityserver.com/identity/connect/authorize',
+
+//   // Login Url of the Identity Provider
+//   logouturl: 'https://demo.identityserver.com/identity/connect/endsession',
+
+
+//   // URL of the SPA to redirect the user to after login
+//   redirectUri: window.location.origin + '/dashboard.html',
+
+//   // The SPA's id. The SPA is registerd with this id at the auth-server
+//   clientId: 'billing_demo',
+
+//   // set the scope for the permissions the client should request
+//   // The first three are defined by OIDC. Also provide user sepecific
+//   scope: 'openid profile email billing_demo_api',
+
 
 // import { AuthConfig } from 'angular-oauth2-oidc';
 
