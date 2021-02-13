1e84e6606381f714cd7e302fea4462e96272b10a
HatemAbid <hatem.imp@gmail.com>
2021-02-13 12:14:11 +0100

    angular-oauth2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { OAuthService } from 'angular-oauth2-oidc';
+import { filter } from 'rxjs/operators';
+import { authCodeFlowConfig } from './auth.config';
+import { JwksValidationHandler } from 'angular-oauth2-oidc-jwks';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bibliotheque';
+
+  constructor(private oauthService: OAuthService) {
+    this.oauthService.configure(authCodeFlowConfig);
+    this.oauthService.loadDiscoveryDocumentAndLogin();
+
+    //this.oauthService.setupAutomaticSilentRefresh();
+
+    // Automatically load user profile
+    this.oauthService.events
+      .pipe(filter(e => e.type === 'token_received'))
+      .subscribe(_ => this.oauthService.loadUserProfile());
+  }
+
+  get userName(): string {
+    const claims = this.oauthService.getIdentityClaims();
+    if (!claims) return null;
+    return claims['given_name'];
+  }
+
+  get idToken(): string {
+    return this.oauthService.getIdToken();
+  }
+
+  get accessToken(): string {
+    return this.oauthService.getAccessToken();
+  }
+
+  refresh() {
+    this.oauthService.refreshToken();
+  }
 }
