ac42c98cc20cdf83a22f0fe245b55499549a75e8
HatemAbid <hatem.imp@gmail.com>
2021-02-19 12:33:16 +0100

    WIP-CATEGORIE-01

---
+++ src/app/model/categorie.ts
@@ -0,0 +1,12 @@
+export class Categorie {
+  id: number;
+  titre: string;
+
+  constructor(
+    id: number,
+    titre: string
+  ) {
+    this.id = id;
+    this.titre = titre;
+  }
+}
--- src/app/model/ouvrage.ts
+++ src/app/model/ouvrage.ts
@@ -1,18 +1,22 @@
+import { Categorie } from "./categorie";
 export class Ouvrage {
   id: number;
   titre: string;
   datepublication: string;
   prix: number;
+  categorie: Categorie
 
   constructor(
     id: number,
     titre: string,
     datepublication: string,
-    prix: number
+    prix: number,
+    categorie: Categorie
   ) {
     this.id = id;
     this.titre = titre;
     this.datepublication = datepublication;
     this.prix = prix;
+    this.categorie = categorie;
   }
 }
